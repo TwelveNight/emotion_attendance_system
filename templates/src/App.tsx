5b3df81aa64aa63e74296f2da101840d87507489
TwelveNight <1315640046@qq.com>
2024-06-07 21:58:38 +0800

    api: 对接接口

--- templates/src/App.tsx
+++ templates/src/App.tsx
@@ -1,9 +1,9 @@
-import { FC, useCallback } from "react";
+import {FC, useCallback, useState} from "react";
 import css from "./app.module.css";
 import { Button, Input, Select } from "antd";
 import RightStatus from "./components/RightStatus/RightStatus";
 import StatusTable from "./components/StatusTable/StatusTable";
-
+import faceApi from "./network/api";
 const mockUserStatusInfoList:UserStatusInfoType[] = [
   {
     name: "叶墨沫",
@@ -59,9 +59,39 @@ const mockUserStatusInfoList:UserStatusInfoType[] = [
     isSmile: true,
     emotions: true,
   },
+     {
+    name: "叶墨沫",
+    times: "2021-09-01",
+    isSmile: true,
+    emotions: true,
+  },
+  {
+    name: "叶墨沫",
+    times: "2021-09-01",
+    isSmile: true,
+    emotions: true,
+  },
+  {
+    name: "叶墨沫",
+    times: "2021-09-01",
+    isSmile: true,
+    emotions: true,
+  },
+  {
+    name: "叶墨沫",
+    times: "2021-09-01",
+    isSmile: true,
+    emotions: true,
+  },
 ]
 
 const App: FC = () => {
+  const [checkInValue,setCheckInValue] = useState('pkl')
+  const checkIn = useCallback(async ()=>{
+    const response = await faceApi.checkIn(checkInValue)
+    console.log(response)
+  },[checkInValue])
+  
   const renderLeft = useCallback(() => {
     return (
       <div className={css.leftBox}>
@@ -72,14 +102,17 @@ const App: FC = () => {
                 style={{
                   width: 100,
                 }}
-                defaultValue={"pkl"}
+                defaultValue={checkInValue}
+                onChange={(value)=>{
+                    setCheckInValue(value)
+                }}
                 options={[
                   { label: "pkl", value: "pkl" },
                   { label: "deepface", value: "deepface" },
                   { label: "h5", value: "h5" },
                 ]}
               ></Select>
-              <Button type="primary">开始录入</Button>
+              <Button type="primary" onClick={checkIn}>开始录入</Button>
           </div>
           <div className={css.checkBox}>
               <p>录出模式:</p>
@@ -104,12 +137,15 @@ const App: FC = () => {
       </div>
     );
   }, []);
-
+  
   const renderImage = useCallback(() => {
     return (
       <div className={css.rightBox}>
-        <div>
-          <img src="{{ url_for('video_feed') }}" width="500rem" height="375rem"></img>
+        <div style={{
+          borderRadius:'20px',
+          overflow:'hidden',
+        }}>
+          <img src="http://127.0.0.1:8088/video_feed" width="800px" height="600px"></img>
         </div>
       </div>
     );
